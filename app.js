d80f8db32abc03fb51ae0559cb28b39937f920d4
Annesa Tran <annesat2020@gmail.com>
2022-06-21 10:40:22 -0700

    Express server setup

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const morgan = require("morgan")
+
 const app = express();
 
 module.exports = app;
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send({"ping":"pong"})
+})
