2910a5e9f782c03db0a8446c90abd66f940ddfb6
Annesa Tran <annesat2020@gmail.com>
2022-06-21 17:05:39 -0700

    Fixed error handling issues

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require("express");
 const morgan = require("morgan")
 const router = require("./routes/gift-exchange")
+const { NotFoundError } = require("./utils/errors")
 
 const app = express();
 
 module.exports = app;
 
+// MIDDLEWARE
+
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use("/gift-exchange", router)
 
-
 app.get("/", (req, res) => {
-    res.status(200).send({"ping":"pong"})
+  res.status(200).send({"ping":"pong"})
 })
 
 router.get("/gift-exchange", (req, res) => {
-    res.status(200).json(res.body);
+  res.status(200).json(res.body);
+})
+
+// Server 404 handler
+app.use((req, res, next) => {
+  return next(new NotFoundError())
+})
+
+// General error handler
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || "Something went wrong in the application";
+  return res
+    .status(status)
+    .json( {"error": {"status": status || 500, "message": message }})
 })
--- utils/errors.js
+++ utils/errors.js
@@ -1,16 +1,21 @@
 class ExpressError extends Error {
   constructor(message, status) {
+    super()
     this.message = message;
     this.status = status;
   };
 };
 
 class BadRequestError extends ExpressError {
-  constructor(message="Bad request", status=400);
+  constructor(message="Bad request") {
+    super(message, 400)
+  };
 };
 
 class NotFoundError extends ExpressError {
-  constructor(message="Not found", status=404);
+  constructor(message="Not found") {
+    super(message, 404)
+  };
 };
 
 module.exports = { ExpressError, BadRequestError, NotFoundError }
