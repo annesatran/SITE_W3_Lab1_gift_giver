71bf21fb548d7b19c701e24b35bc2c9d365e0a39
Annesa Tran <annesat2020@gmail.com>
2022-06-21 17:14:28 -0700

    Edited algorithms to properly catch errors

--- models/gift-exchange.js
+++ models/gift-exchange.js
@@ -2,35 +2,12 @@ const { BadRequestError } = require("../utils/errors")
 
 class GiftExchange {
 
-  // HELPER FUNCTIONS
-  static getRandomIndex(maxIndex) {
-    return Math.floor(Math.random() * maxIndex);
-  }
-  
-  static  randomizeArray(array) {
-    let currentIndex = array.length, randomIndex;
-
-    // while there are still items left to shuffle,
-    while (currentIndex != 0) {
-  
-      // get a random name
-      randomIndex = getRandomIndex(currentIndex);
-      currentIndex -= 1;
-  
-      // swap the random name with the current name
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
-  
-
-  // STATIC METHODS FOR LAB
-
   static pairs(names) {
-    // if there's an odd number of names, throw an error
-    if (names.length % 2 == 1) {
+    // error handling
+    if (!(names) || names.length === 0) {
+      throw new BadRequestError("Must enter names")
+    }
+    else if (names.length % 2 === 1) {
       throw new BadRequestError("The number of names cannot be odd");
     }
     // create new copy of the names list
@@ -39,10 +16,10 @@ class GiftExchange {
 
     while (tempNames.length > 0) {
 
-      const firstIndex = getRandomIndex(tempNames.length);
+      const firstIndex = Math.floor(Math.random() * tempNames.length)
       const firstName = tempNames.splice(firstIndex, 1)[0];
 
-      const secondIndex = getRandomIndex(tempNames.length);
+      const secondIndex = Math.floor(Math.random() * tempNames.length)
       const secondName = tempNames.splice(secondIndex, 1)[0];
 
       pairings.push([firstName, secondName]);
@@ -52,26 +29,28 @@ class GiftExchange {
   }
 
   static traditional(names) {
+    // error handling
+    if (!(names) || names.length === 0) {
+      throw new BadRequestError("Must enter names")}
+
     let tempNames = [... names];
     let pairings = [];
     
     // randomize order of the array
-    tempNames = randomizeArray(tempNames)
+    tempNames.sort(() => Math.random() - 0.5);
 
     // chain together names
     for (let i = 0; i < tempNames.length; i++) {
-      
       // get name at current index
       const firstName = tempNames[i];
-
       // if firstName is not the last one, set secondName as the one right after it, else set it to the very first name
       const secondName = (!(i === tempNames.length - 1)) ? tempNames[i + 1] : tempNames[0];
       
       pairings.push(`${firstName} is giving a gift to ${secondName}`)
     }
 
-    return pairs
+    return pairings
   }
 }
 
-export default GiftExchange
+module.exports = GiftExchange
--- routes/gift-exchange.js
+++ routes/gift-exchange.js
@@ -1,12 +1,27 @@
-const express = require("express")
-const router = express.Router()
+const GiftExchange = require("../models/gift-exchange");
+const express = require("express");
+const router = express.Router();
 
 module.exports = router;
 
-router.post("/pairs", async (req, res) => {
-    res.status(200).json(req.body);
+router.post("/pairs", async (req, res, next) => {
+    try {
+        const results = await GiftExchange.pairs(req.body.names);
+        // res.status(200).json(results);
+        res.status(200).send(results)
+        }
+    catch (err) {
+        next(err);
+    }
 });
 
-router.post("/traditional", async (req, res) => {
-    res.status(200).json(req.body)
+router.post("/traditional", async (req, res, next) => {
+    try {
+        const results = await GiftExchange.traditional(req.body.names);
+        //res.status(200).json(results);
+        res.status(200).send(results)
+        }
+    catch (err) {
+        next(err);
+    }
 });
